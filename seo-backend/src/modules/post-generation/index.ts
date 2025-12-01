864f7b18ccfdf7ae44ed053e34fcaae70a0cb2ed
anon <anon@myzone>
2025-12-01 15:31:22 +0200

    nah bro let's deprecate this shit

--- seo-backend/src/modules/post-generation/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './interfaces/post-generation-job.interface';
-export * from './processors/post-generation.processor';
-export * from './queue.constants';
-export * from './queue.module';
-export * from './services/queue.service';
-
-
